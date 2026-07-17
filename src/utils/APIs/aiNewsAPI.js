aacf68521e0052fc2296891a74f324616cdcdf8a
Suchith Reddy <suchithreddykaila@gmail.com>
2026-07-17 20:28:30 +0530

    feat: add reusable AI News category filter component

---
+++ src/utils/APIs/aiNewsAPI.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import {
+  API_CONFIG,
+  requestErrorInterceptor,
+  requestInterceptor,
+  responseErrorInterceptor,
+  responseInterceptor,
+} from './interceptors';
+
+const api = axios.create(API_CONFIG);
+
+api.interceptors.request.use(requestInterceptor, requestErrorInterceptor);
+api.interceptors.response.use(responseInterceptor, responseErrorInterceptor);
+
+export const aiNewsAPI = {
+  /**
+   * Fetch available categories dynamically from the backend.
+   * GET /api/ai-news/ainews/categories
+   */
+  getCategories: async () => {
+    const response = await api.get('/ai-news/ainews/categories');
+    return response.data;
+  },
+};
+
+export default aiNewsAPI;
