4bb5b3f659f812542e69c669affac5349774cd3a
Bhavani Bandreddy <bhavanibandreddy13@gmail.com>
2026-08-01 07:13:51 +0530

    fix(build): resolve Linux case-sensitive ProfileSection import

--- src/components/pages/Profile/profileSettings/ProfileSettings.jsx
+++ src/components/pages/Profile/profileSettings/ProfileSettings.jsx
@@ -10,7 +10,7 @@ import AppearanceSection from './AppearanceSection';
 import PreferencesSection from './PreferencesSection';
 import SavedSection from './SavedSection';
 import AccountSecurity from './AccountSecurity';
-import ProfileSection from './profileSection';
+import ProfileSection from './ProfileSection';
 import { updateUser as updateUserSlice } from '@/services/auth/authSlice';
 import { usersAPI } from '@/utils/APIs/userAPI';
 import { authAPI } from '@/utils/APIs/authAPI';
